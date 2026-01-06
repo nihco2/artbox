648a25cd1c5f6e7e0537013a5134871cceb8ee57
Nico <nihco2@gmail.com>
2026-01-06 18:46:58 +0100

    add:connect each oeuvre to bdd

--- oeuvre.php
+++ oeuvre.php
@@ -1,22 +1,20 @@
 <?php
     require 'header.php';
-    require 'oeuvres.php';
+    // require 'oeuvres.php';
+    require 'bdd.php';
+    $mysqlClient = connexion();
 
     // Si l'URL ne contient pas d'id, on redirige sur la page d'accueil
     if(empty($_GET['id'])) {
         header('Location: index.php');
     }
 
-    $oeuvre = null;
-
-    // On parcourt les oeuvres du tableau afin de rechercher celle qui a l'id précisé dans l'URL
-    foreach($oeuvres as $o) {
-        // intval permet de transformer l'id de l'URL en un nombre (exemple : "2" devient 2)
-        if($o['id'] === intval($_GET['id'])) {
-            $oeuvre = $o;
-            break; // On stoppe le foreach si on a trouvé l'oeuvre
-        }
-    }
+    // Récupération de l'oeuvre en fonction de son id
+    $sqlQuery = $mysqlClient->prepare('SELECT * FROM oeuvres WHERE id = ?');
+    $sqlQuery->execute([
+       $_GET['id']
+    ]);
+    $oeuvre = $sqlQuery->fetch();
 
     // Si aucune oeuvre trouvé, on redirige vers la page d'accueil
     if(is_null($oeuvre)) {
